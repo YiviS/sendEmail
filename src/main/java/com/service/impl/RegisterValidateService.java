5712bacab2c92d2aec6a674c310023a43b7e8f4d
xuguang93@qq.com <z1598753>
2016-07-21 11:19:41 +0800

    发送邮件小工具 version 1.0.1

--- src/main/java/com/service/impl/RegisterValidateService.java
+++ src/main/java/com/service/impl/RegisterValidateService.java
@@ -68,22 +68,21 @@ public class RegisterValidateService implements IRegisterValidateService{
     public void processActivate(String email , String validateCode)throws ServiceException, ParseException{
         //数据访问层，通过email获取用户信息
         User user= userDao.selectByEmail(email);
-        System.out.println("==user==="+user);
         //验证用户是否存在
         if(user!=null) {
             //验证用户激活状态
             if(user.getStatus()==0) {
                 ///没激活
                 Date currentTime = new Date();//获取当前时间
+                Date sqldate = user.getRegistertime();
                 //验证链接是否过期
                 currentTime.before(user.getRegistertime());
-                if(currentTime.before(user.getRegistertime())) {
+                long dd = currentTime.getTime()-user.getRegistertime().getTime();
+                if(dd/(1000*60*60*24)<2) {
                     //验证激活码是否正确
                     if(validateCode.equals(user.getValidatecode())) {
                         //激活成功， //并更新用户的激活状态，为已激活
-                        System.out.println("==sq==="+user.getStatus());
                         user.setStatus(1);//把状态改为激活
-                        System.out.println("==sh==="+user.getStatus());
                         userDao.updateByEmail(user);
                     } else {
                         throw new ServiceException("激活码不正确");
